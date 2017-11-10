2d3dfa704620edbacf1637581caa61b1c16e5563
boli <965556835@qq.com>
2017-11-10 13:38:09 +0800

    2

---
+++ java-study/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+C:\Program Files\Java\jdk-9.0.1\bin
+D:\java\mygithub\java\java-study\src
+一个源文件中最多有一个public类
+ */
+
+/**
+ *
+ * @author bo.li
+ * 
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
+
+class t {}

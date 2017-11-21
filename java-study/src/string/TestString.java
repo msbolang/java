eca630650da3a7526260aec2686e3838dfc9f9ae
msbolang <965556835@qq.com>
2017-11-21 23:04:35 +0800

    判断几个大小写字母

---
+++ java-study/src/string/TestString.java
@@ -0,0 +1,27 @@
+
+/**
+ *判断几个大写字母 几个小写字母 几个非字母
+ * @author Administrator
+ */
+
+
+class TestString  {
+    public static void main(String[] args) {
+        String s = "AaaaBdddCkkkfjdjaks3232442l";
+        System.out.println(s.length());
+        int Slength = s.length();
+        int D = 0;
+        int S = 0;
+        int DF = 0;
+        String ss = "AAAA";
+        for(int i = 0;i < Slength;i++) {
+//            System.out.println(s.charAt(i));
+             System.out.println(s.charAt(i));
+        
+//            if(s.charAt(i)==)
+        }
+        
+        System.out.println(ss.toLowerCase​());System.out.println(ss.toLowerCase​());System.out.println(ss.toLowerCase​());
+    }
+
+}
